893d4a467fb2434362c558dc7eccc049c9028b6e
Savvy <malcomeqgreen@gmail.com>
2018-02-22 22:04:31 -0500

    Economy Accounts now use BigDecimal rather than double

--- src/main/java/me/savvy/main/account/Account.java
+++ src/main/java/me/savvy/main/account/Account.java
@@ -1,33 +1,38 @@
 package me.savvy.main.account;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Account {
 
   private final UUID accountOwner;
-  private double balance;
+  private BigDecimal balance;
 
   public Account(UUID accountOwner) {
     this.accountOwner = accountOwner;
   }
 
-  public double getBalance() {
+  public BigDecimal getBalance() {
     return this.balance;
   }
 
   public void addToBalance(double amount) {
-    this.setBalance(this.getBalance() + amount);
+    this.setBalance(this.balance.add(this.toBigDecimal(amount)));
   }
 
   public void removeFromBalance(double amount) {
-    this.setBalance(this.getBalance() - amount);
+    this.setBalance(this.balance.subtract(this.toBigDecimal(amount)));
   }
 
-  public void setBalance(double balance) {
+  public void setBalance(BigDecimal balance) {
     this.balance = balance;
   }
 
   public UUID getAccountOwner() {
     return this.accountOwner;
+  }
+
+  private BigDecimal toBigDecimal(double amount) {
+    return BigDecimal.valueOf(amount);
   }
 }
