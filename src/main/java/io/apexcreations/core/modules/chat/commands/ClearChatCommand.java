583bc88140d92d68a854647bd7702c7033182b72
Venal <venaldev@gmail.com>
2018-03-09 20:24:24 -0500

    Formatting chat commands differently, added arguments for spawn command and clear chat, removed subcommand classes

--- src/main/java/io/apexcreations/core/modules/chat/commands/ClearChatCommand.java
+++ src/main/java/io/apexcreations/core/modules/chat/commands/ClearChatCommand.java
@@ -2,8 +2,8 @@ package io.apexcreations.core.modules.chat.commands;
 
 import io.apexcreations.core.builders.MessageBuilder;
 import io.apexcreations.core.commands.ApexCommand;
-import io.apexcreations.core.modules.chat.commands.sub.ClearChatSubCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -12,16 +12,31 @@ public class ClearChatCommand extends ApexCommand {
   public ClearChatCommand(String name, String description, String permission, boolean playerOnly,
       String... aliases) {
     super(name, description, permission, playerOnly, aliases);
-    this.getApexCore().getSubCommandCache().add("clear" ,new ClearChatSubCommand("Clear chat", "Clears chat", "apex.clearchat", false));
   }
 
   @Override
   public boolean executeCommand(CommandSender commandSender, String label, String[] args) {
-
-    Bukkit.getOnlinePlayers().stream().filter(player ->
-        (!player.hasPermission("apex.clearchat.exempt"))).forEach(this::clearChat);
-    MessageBuilder.create("&aChat has been cleared!").send(commandSender);
-    return false;
+      if (args.length == 0) {
+          Bukkit.getOnlinePlayers().stream().filter(player ->
+                  (!player.hasPermission("apex.clearchat.exempt"))).forEach(this::clearChat);
+          MessageBuilder.create("&aChat has been cleared!").send(commandSender);
+          return false;
+      }
+      Player target = Bukkit.getPlayer(args[0]);
+      
+      if (target == null) {
+          MessageBuilder.create("&4Target player not found").send(commandSender);
+          return true;
+      }
+      // Wouldn't want a mod to clear an admin's chat
+      if (target.hasPermission("apex.clearchat.exempt")) {
+          MessageBuilder.create("&4You cannot clear this player's chat!").send(commandSender);
+          return true;
+      }
+      clearChat(target);
+      MessageBuilder.create("&2Your chat has been cleared!").send(target);
+      MessageBuilder.create(String.format("%s&2's chat has been cleared", ChatColor.YELLOW + target.getName()));
+      return true;
   }
 
   private void clearChat(Player player) {
---
+++ src/main/java/io/apexcreations/core/modules/chat/commands/sub/ChatStopCommand.java
@@ -0,0 +1,4 @@
+package io.apexcreations.core.modules.chat.commands.sub;
+
+public class ChatStopCommand {
+}
